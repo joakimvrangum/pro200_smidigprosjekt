26cd50c96e14b16a96c1d50ad6a03ff34cbd2aa1
unknown <joakimvrangum@gmail.com>
2018-05-23 13:01:48 +0100

    Endret BarcodeScannerTest. Mulig fremskritt.

--- NodeMCUTest/BarcodescannerTest/BarcodescannerTest.ino
+++ NodeMCUTest/BarcodescannerTest/BarcodescannerTest.ino
@@ -1,54 +1,34 @@
 #include <SoftwareSerial.h>
 
-const int pinRX = 0;
-const int pinTX = 1;
+const int pinRX = 12;
+const int pinTX = 13;
 const int pinBarcodeTrigger = 8;
 
 String barcodeValue;
-String readString;
-int x = 1;
 
-SoftwareSerial mySerial(0, 1); // RX, TX
+SoftwareSerial mySerial(pinRX, pinTX, false); // RX, TX
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  mySerial.begin(9600);
   pinMode(pinBarcodeTrigger, OUTPUT);
-//  byte message[] = {0x0B, 0x04, 0x31, 0x00, 0x00, 0x41, 0x30, 0x30, 0x30, 0x30, 0xFF, 0xFD, 0xC0};
-//  mySerial.write(message, sizeof(message));
+  pinMode(pinRX, INPUT);
+  pinMode(pinTX, OUTPUT);
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  byte message[] = {0x0B, 0x04, 0x31, 0x00, 0x00, 0x41, 0x30, 0x30, 0x30, 0x30, 0xFF, 0xFD, 0xC0};
+  if(mySerial.write(message, sizeof(message))) {
+    Serial.print("Skrevet melding.");
+  }
 }
 
 void loop() {
-  Serial.println("Ferdig med setup");
-
   digitalWrite(pinBarcodeTrigger, HIGH);
-  delay(20);
+  delay(30);
   digitalWrite(pinBarcodeTrigger, LOW);
-  if (Serial.available()) {
-    Serial.println("Serial er available");
-    Serial.println((char)mySerial.read());
-    //byte x = mySerial.read();
-    //Serial.println(x, DEC);
+  if (mySerial.available()) {
+    Serial.print(mySerial.read());
   }
-  delay(1000);
-//  
-//  if (x == 1) { //run the loop once
-//
-//    byte message[] = {0x0A, 0x04, 0x31, 0x00, 0x00, 0x59, 0x4c, 0x54, 0x4b, 0xFF, 0xFD, 0x7E};
-//    mySerial.write(message, sizeof(message));
-//    Serial.println("sendt message");
-//    for (int i = 0; i < 40; i++) {
-//      Serial.print(i);
-//      Serial.println(" venter på serial");
-//      while (!mySerial.available()); // wait for a character
-//      
-//      char incomingByte = mySerial.read();
-//      Serial.print(incomingByte);
-//      Serial.print(' ');
-//    }
-//    
-//    Serial.println("ferdig");
-//    x = 0;
-//  }
+  delay(200);
 }
