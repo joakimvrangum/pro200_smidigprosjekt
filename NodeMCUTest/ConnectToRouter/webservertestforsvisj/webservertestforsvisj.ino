8eff866c2389b8395501e6a73c69cf2a4ffb3f99
Hekkii <hekkiiwalters@gmail.com>
2018-06-04 12:25:30 +0200

    Testing everything

--- NodeMCUTest/ConnectToRouter/webservertestforsvisj/webservertestforsvisj.ino
+++ NodeMCUTest/ConnectToRouter/webservertestforsvisj/webservertestforsvisj.ino
@@ -12,7 +12,7 @@ const char *APPASS = "";
 
 const byte DNS_PORT = 53;
 
-bool wificonnected;
+bool wificonnected = false;
 
 //IPAddress ip(192,168,4,1);
 //IPAddress gateway(192,168,1,1);
@@ -28,82 +28,103 @@ void setup() {
   //WRITE TO OR READ FROM EEPROM
   EEPROM.begin(1024);
 
+  int address = 0;
+  char winame[32];
+  char wipass[64];
+  EEPROM.get(address, winame);
+  address = 100;
+  EEPROM.get(address, wipass);
+
+  WiFi.begin(winame, wipass);
+  int count = 0;
+  while (WiFi.status() != WL_CONNECTED && count < 60) {
+
+    delay(500);
+    count++;
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    wificonnected = true;
+  } else {
+    wificonnected = false;
+
+    WiFi.softAP(APID, APPASS);
+
+
+    IPAddress APIP = WiFi.softAPIP();
+    dnsServer.start(DNS_PORT, "*", WiFi.softAPIP());
+
+    Serial.println("APIP er");
+    Serial.println(APIP);
+    server.on("/", handleRoot);
+    server.on("/input", getInputs);
+    server.begin();
+  }
+
   //TODO READ FROM EEPROM TO CHECK IF THERE IS A WIFINAME AND PASSWORD
-  wificonnected = false;
 
 
 
   //WiFi.mode(WIFI_AP);
   //WiFi.config(ip, gateway, subnet, dns);
-  WiFi.softAP(APID, APPASS);
 
 
-  IPAddress APIP = WiFi.softAPIP();
-  dnsServer.start(DNS_PORT, "*", WiFi.softAPIP());
+  /*
+    String ssid = "test";
+    String password = "smaltansikt";
 
-  Serial.println("APIP er");
-  Serial.println(APIP);
-  server.on("/", handleRoot);
-  server.on("/input", getInputs);
-  server.begin();
+    int address = 0;
+    char arrayToStore[64];
+    char arrayThatWasStored[64];
+    ssid.toCharArray(arrayToStore, ssid.length()+1);
+    Serial.print("Writing this to EEPROM : ");
 
-  
-  String ssid = "test";
-  String password = "smaltansikt";
+    Serial.println(arrayToStore);
 
-  int address = 0;
-  char arrayToStore[64];
-  char arrayThatWasStored[64];
-  ssid.toCharArray(arrayToStore, ssid.length()+1);
-  Serial.print("Writing this to EEPROM : ");
-
-  Serial.println(arrayToStore);
-  
-  EEPROM.put(address, arrayToStore);
-  EEPROM.get(address, arrayThatWasStored);
-  Serial.print("Got this from EEPROM : ");
-
-  Serial.println(arrayThatWasStored);
-
-
-  address = 32;
-  password.toCharArray(arrayToStore, password.length()+1);
-  Serial.print("Writing this to EEPROM : ");
-  Serial.println(arrayToStore);
-
-  
-  EEPROM.put(address, arrayToStore);
-  EEPROM.get(address, arrayThatWasStored);
-  Serial.print("Got this from EEPROM : ");
+    EEPROM.put(address, arrayToStore);
+    EEPROM.get(address, arrayThatWasStored);
+    Serial.print("Got this from EEPROM : ");
 
     Serial.println(arrayThatWasStored);
 
 
-  
-  //writeEEPROM(0, ssid);
-  Serial.println();
-  //writeEEPROM(32, password);
-  Serial.println();
-  Serial.println("Wrote to EEPROM, let's check if it actually did anything");
-
-  //SSIDName = readEEPROM(0,32);
-  //SSIDPassword = readEEPROM(32, 64);
-
-  Serial.println();
-  //Serial.println(SSIDName);
-  //Serial.println(SSIDPassword);
-
-  
-/*
-  String winame = readEEPROM(0, 32);
-  Serial.println("We got this from EEPROM as name:");
-  Serial.println(winame);
+    address = 32;
+    password.toCharArray(arrayToStore, password.length()+1);
+    Serial.print("Writing this to EEPROM : ");
+    Serial.println(arrayToStore);
 
 
-  String wipassword = readEEPROM(32, 64);
-  Serial.println("We got this from EEPROM as password:");
-  Serial.println(wipassword);
-*/
+    EEPROM.put(address, arrayToStore);
+    EEPROM.get(address, arrayThatWasStored);
+    Serial.print("Got this from EEPROM : ");
+
+    Serial.println(arrayThatWasStored);
+
+
+
+    //writeEEPROM(0, ssid);
+    Serial.println();
+    //writeEEPROM(32, password);
+    Serial.println();
+    Serial.println("Wrote to EEPROM, let's check if it actually did anything");
+
+    //SSIDName = readEEPROM(0,32);
+    //SSIDPassword = readEEPROM(32, 64);
+
+    Serial.println();
+    //Serial.println(SSIDName);
+    //Serial.println(SSIDPassword);
+
+
+
+    String winame = readEEPROM(0, 32);
+    Serial.println("We got this from EEPROM as name:");
+    Serial.println(winame);
+
+
+    String wipassword = readEEPROM(32, 64);
+    Serial.println("We got this from EEPROM as password:");
+    Serial.println(wipassword);
+  */
 }
 
 void writeEEPROM(int startOfMemoryToAppend, String stringToAppend) {
@@ -140,9 +161,20 @@ String readEEPROM(int startOfMemoryToRead, int lengthToRead) {
 
 
 void loop() {
-  dnsServer.processNextRequest();
-  server.handleClient();
-  delay(1);
+  if (!wificonnected) {
+    dnsServer.processNextRequest();
+    server.handleClient();
+    delay(1);
+  } else {
+    delay(1000);
+    //wificonnected = checkConnection();
+  }
+
+}
+
+bool checkConnection(){
+  if(WiFi.status() == WL_CONNECTED) return true;
+  return false;
 }
 
 void getInputs() {
@@ -150,110 +182,78 @@ void getInputs() {
   //Eller så har data blitt submittet
   //Må i så fall hente data fra siden
   Serial.println("We are outside of if statement");
-  /*
-    if (!wificonnected && server.arg("wifiname") != "") {
-      String ssid = server.arg("wifiname");
-      String password = "";
-      Serial.print("Wifi is called ");
-      Serial.println(ssid);
-      Serial.println("Checking if there was a password input");
-      if (server.arg("wifipass") != "") {
-        password = server.arg("wifipass");
-        Serial.print("There is a password and it is: ");
-        Serial.println(password);
+
+  if (!wificonnected && server.arg("wifiname") != "") {
+    String ssid = server.arg("wifiname");
+    String password = "";
+    Serial.print("Wifi is called ");
+    Serial.println(ssid);
+    Serial.println("Checking if there was a password input");
+    if (server.arg("wifipass") != "") {
+      password = server.arg("wifipass");
+      Serial.print("There is a password and it is: ");
+      Serial.println(password);
+    }
+
+    char bufid[32];
+    char passid[64];
+    ssid.toCharArray(bufid, ssid.length() + 1);
+    password.toCharArray(passid, password.length() + 1);
+
+    //Lager en liste over nettverk
+    int networks = WiFi.scanNetworks();
+
+    for (int i = 0; i < networks; i++) {
+      Serial.print("Name of network : ");
+      Serial.println(WiFi.SSID(i));
+      Serial.print("Strength : ");
+      Serial.println(WiFi.RSSI(i));
+      //Passord eller ikke
+      Serial.print("Encrypted with : ");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      bool encrypted = (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? false : true;
+      if (ssid == WiFi.SSID(i)) {
+
+        Serial.println("NETWORK FOUND");
+        if (encrypted) {
+          Serial.println("Password needed");
+          if (password != "") {
+            WiFi.begin(bufid, passid);
+            int count = 0;
+            while (WiFi.status() != WL_CONNECTED && count < 60) {
+              delay(500);
+              Serial.println("connecting");
+              count++;
+            }
+            if (WiFi.status() == WL_CONNECTED) {
+              wificonnected = true;
+              Serial.println("Connection successful, saving password to EEPROM");
+
+              int address = 0;
+              EEPROM.put(address, bufid);
+              address = 100;
+              EEPROM.put(address, passid);
+            }
+
+          } else {
+            Serial.println("You need to provide a password");
+          }
+        } else {
+          Serial.println("You dont need a password here, but you do need to add some code to connect to wifi");
+        }
+
+      } else {
+        Serial.println("Not the right network");
       }
 
-      char bufid[32];
-      char passid[64];
-      ssid.toCharArray(bufid, sizeof(ssid));
-      password.toCharArray(passid, sizeof(password));
-
-      writeEEPROMchar(0, bufid);
-      writeEEPROMchar(32, passid);
-      Serial.println("Wrote to EEPROM, let's check if it actually did anything");
-      //char *winame = readEEPROM(0, 32);
-      Serial.println("We got this from EEPROM as name:");
-      Serial.println(winame);
-
-      free(winame);
-
-      //char *wipassword = readEEPROM(32, 64);
-
-      Serial.println("We got this from EEPROM as password:");
-      Serial.println(wipassword);
-      free(wipassword);
-
-
-        //Lager en liste over nettverk
-        int networks = WiFi.scanNetworks();
-
-        for (int i = 0; i < networks; i++) {
-        Serial.print("Name of network : ");
-        Serial.println(WiFi.SSID(i));
-        Serial.print("Strength : ");
-        Serial.println(WiFi.RSSI(i));
-        //Passord eller ikke
-        Serial.print("Encrypted with : ");
-        Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
-        bool encrypted = (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? false : true;
-        if (ssid == WiFi.SSID(i)) {
-
-          Serial.println("NETWORK FOUND");
-          if (encrypted) {
-            Serial.println("Password needed");
-            if (password != "") {
-              char bufid[32];
-              char passid[64];
-              ssid.toCharArray(bufid, sizeof(ssid));
-              password.toCharArray(passid, sizeof(password));
-              WiFi.begin(bufid, passid);
-              int count = 0;
-              while (WiFi.status() != WL_CONNECTED && count < 60) {
-                delay(500);
-                Serial.println("connecting");
-                count++;
-              }
-              if (WiFi.status() == WL_CONNECTED) {
-                wificonnected = true;
-                Serial.println("Connection successful, saving password to EEPROM");
-                //Dette funker ikke, hvorfor fungerer det ikke?
-                //readEEPROM returnerer en char pointer som peker til adressen til char *string deklarert inni readEEPROM
-                //når jeg kjører først én readEEPROM og så bruker dataen til å skrive ut via serial print går det fint
-                //Men hvis man kjører readEEPROM to ganger på rad, uten å skrive ut vil den første variablen som returneres
-                //overskrives av den neste readEEPROM fordi de begge peker til samme adresser.
-                //Med andre ord kan den ikke returnere en char* ELLER så må man bare bruke dataene før man leser neste.
-                writeEEPROMchar(0, bufid);
-                writeEEPROMchar(32, passid);
-                Serial.println("Wrote to EEPROM, let's check if it actually did anything");
-                char *winame = readEEPROM(0, 32);
-
-                Serial.println("We got this from EEPROM as name:");
-                Serial.println(winame);
-                char *wipassword = readEEPROM(32, 64);
-
-                Serial.println("We got this from EEPROM as password:");
-                Serial.println(wipassword);
-
-                Serial.println("BREAKING");
-              }
-
-            } else {
-              Serial.println("You need to provide a password");
-            }
-          }
-
-        } else {
-          Serial.println("Not the right network");
-        }
-
-        Serial.println();
-        Serial.println();
-        }
-        if (!wificonnected) Serial.println("Could not find the network");
-
-
+      Serial.println();
+      Serial.println();
     }
-  */
+    if (!wificonnected) Serial.println("Could not find the network");
+
+
+  }
+
 
   Serial.println("Inputs updated");
 }
@@ -267,7 +267,7 @@ void handleRoot() {
 }
 
 /*
-void writeEEPROMchar(int startOfMemoryToAppend, char * stringToAppend, int lengde) {
+  void writeEEPROMchar(int startOfMemoryToAppend, char * stringToAppend, int lengde) {
   Serial.println("We are inside write function and this is what we are writing:");
 
   Serial.println(stringToAppend);
@@ -288,7 +288,7 @@ void writeEEPROMchar(int startOfMemoryToAppend, char * stringToAppend, int lengd
   EEPROM.commit();
   delay(2000);
   Serial.println();
-}
+  }
 */
 
 
