555c3ba4d73ae584140bc74553fce2407321364b
Jaran Bjørtomt <jaran1994@gmail.com>
2018-06-11 11:56:34 +0200

    Fjernet unødvendig kode

--- NodeMCU_kode/MASTER/MASTER.ino
+++ NodeMCU_kode/MASTER/MASTER.ino
@@ -7,9 +7,6 @@
 #include <SoftwareSerial.h>
 #include <WiFiManager.h>
 
-bool changePass = false;
-bool blinkGreen = false;
-
 const int pinRX = 5;    //5;  // 3
 const int pinTX = 4;    //4;  // 1
 const int pinBarcodeTrigger = 16;
@@ -42,49 +39,49 @@ void loop() {
     Serial.println("Mangler nett. Prøver å sette opp nett igjen.");
     reset();
   }
-    wifiManager.setColor(255, 30, 0);
-    digitalWrite(pinBarcodeTrigger, HIGH);
-    if (mySerial.available()) {
-      delay(400);
-      c = mySerial.read();
-      if ((!blinkGreen) && (barCode.length() > 0) && (barCode.length() < 2)) {
-        wifiManager.setColor(0, 255, 0);
-        delay(500);
-        blinkGreen = true;
-      }
-      if ((int)c == 13) {
-        if (WiFi.status() == WL_CONNECTED) {
-          Serial.print("Sender strekkode til server >> " + barCode + " >> ");
-          HTTPClient http;
-          postData = String("boks_id=") + boksID + "&upc=" + barCode;
-          http.begin("http://www.vrangum.com/kolonial/handlekurv");
-          http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-          int httpCode = http.POST(postData);
-          String response = http.getString();
-          Serial.println("Server >> " + (String) httpCode + " >> " + response);
-          http.end();
-          c = '\0';
-          barCode = "";
-
-          if (response == "OK") {
-            blinkGreen = false;
-            //wifiManager.setColor(0, 255, 0); Lyser grønt om varen blir lagt til i handlekurven
-            delay(500);
-          } else if (response == "FAIL") {
-            blinkGreen = false;
-            wifiManager.setColor(255, 0, 0);
-            delay(3000);
-          } else if (response == "LEVERING BESTILT") {
-            wifiManager.setColor(255, 255, 255);
-            delay(2000);
-          }
-
-        }
-      } else {
-        if ((' ' <= c) && (c <= '~')) barCode += c;
-      }
-    }
+  wifiManager.setColor(255, 30, 0);
+  digitalWrite(pinBarcodeTrigger, HIGH);
+  if (mySerial.available()) {
     delay(400);
+    c = mySerial.read();
+    if ((!blinkGreen) && (barCode.length() > 0) && (barCode.length() < 2)) {
+      wifiManager.setColor(0, 255, 0);
+      delay(500);
+      blinkGreen = true;
+    }
+    if ((int)c == 13) {
+      if (WiFi.status() == WL_CONNECTED) {
+        Serial.print("Sender strekkode til server >> " + barCode + " >> ");
+        HTTPClient http;
+        postData = String("boks_id=") + boksID + "&upc=" + barCode;
+        http.begin("http://www.vrangum.com/kolonial/handlekurv");
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        int httpCode = http.POST(postData);
+        String response = http.getString();
+        Serial.println("Server >> " + (String) httpCode + " >> " + response);
+        http.end();
+        c = '\0';
+        barCode = "";
+
+        if (response == "OK") {
+          blinkGreen = false;
+          //wifiManager.setColor(0, 255, 0); Lyser grønt om varen blir lagt til i handlekurven
+          delay(500);
+        } else if (response == "FAIL") {
+          blinkGreen = false;
+          wifiManager.setColor(255, 0, 0);
+          delay(3000);
+        } else if (response == "LEVERING BESTILT") {
+          wifiManager.setColor(255, 255, 255);
+          delay(2000);
+        }
+
+      }
+    } else {
+      if ((' ' <= c) && (c <= '~')) barCode += c;
+    }
+  }
+  delay(400);
 }
 
 void reset() {
